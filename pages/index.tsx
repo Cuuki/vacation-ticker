4845c73047cbad6ca1a477bbd7eca0af5134d7c2
Patrick Söllner <ps@lottaleben.de>
2019-08-25 19:36:04 +0200

    build(next): add necessary base files for next.js implementation
    issue #8

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+export default class extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Vacation Ticker</h1>
+                <Button variant="contained" color="primary">
+                    Hello World
+                </Button>
+            </div>
+        );
+    }
+}
