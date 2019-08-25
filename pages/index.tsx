b81710304acd64162f987da377c12ad1bc771365
Patrick Söllner <ps@lottaleben.de>
2019-08-25 21:30:18 +0200

    build(css): add postcss libraries and config with example
    closes #8

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,10 +5,22 @@ export default class extends React.Component {
     render() {
         return (
             <div>
-                <h1>Vacation Ticker</h1>
+                <h1><span>Vacation ticker</span></h1>
                 <Button variant="contained" color="primary">
                     Hello World
                 </Button>
+                <style jsx>{`
+                    div {
+                        & h1 {
+                            color: color(#5cb289 tint(50%));
+                        }
+                    }
+                    h1 {
+                        & span {
+                            text-transform: uppercase;
+                        }
+                    }
+                `}</style>
             </div>
         );
     }
