e52dabafb7abbbea93d71a458c909d6cb014a2af
Patrick Söllner <ps@lottaleben.de>
2019-08-30 18:45:44 +0200

    build(common): add styled components with babel conf
    issue #9

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,26 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import styled, {css} from 'styled-components';
+
+const StyledButton = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: color(#5cb289 tint(50%));
+  margin: 0.5em 1em;
+  padding: 0.25em 1em;
+
+  ${props =>
+    props.primary &&
+    css`
+      background: palevioletred;
+      color: white;
+    `}
+`;
+
+const Container = styled.div`
+  text-align: center;
+`;
 
 const Index = () => {
   return (
@@ -10,18 +31,10 @@ const Index = () => {
       <Button variant="contained" color="primary">
         Hello World
       </Button>
-      <style jsx>{`
-        div {
-          & h1 {
-            color: color(#5cb289 tint(50%));
-          }
-        }
-        h1 {
-          & span {
-            text-transform: uppercase;
-          }
-        }
-      `}</style>
+      <Container>
+        <StyledButton>Normal Button</StyledButton>
+        <StyledButton primary>Primary Button</StyledButton>
+      </Container>
     </div>
   );
 };
