f559e5490a7ac934b942b32a7fedb13e03746194
Patrick Söllner <ps@lottaleben.de>
2019-09-29 00:31:16 +0200

    feat(page-layout): add basic structure for countdown ticker
    issue #10

--- components/Countdown/CountdownTicker/CountdownTicker.tsx
+++ components/Countdown/CountdownTicker/CountdownTicker.tsx
@@ -1,5 +1,35 @@
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
 const CountdownTicker: React.FC = () => {
-  return <>CountdownTicker</>;
+  return (
+    <>
+      <Box mb={3} fontFamily="Monospace">
+        <Typography component="p" variant="body2" gutterBottom>
+          00 Years
+        </Typography>
+        <Typography component="p" variant="body2" gutterBottom>
+          00 Months
+        </Typography>
+        <Typography component="p" variant="body2" gutterBottom>
+          00 Days
+        </Typography>
+        <Typography component="p" variant="body2" gutterBottom>
+          00 Hours
+        </Typography>
+        <Typography component="p" variant="body2" gutterBottom>
+          00 Minutes
+        </Typography>
+        <Typography component="p" variant="body2">
+          00 Seconds
+        </Typography>
+      </Box>
+      <Button variant="outlined" color="primary">
+        Clear
+      </Button>
+    </>
+  );
 };
 
 export default CountdownTicker;
