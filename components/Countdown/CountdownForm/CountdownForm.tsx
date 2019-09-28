5269440a1f71aea6a1136ed52237234f6a303f2c
Patrick Söllner <ps@lottaleben.de>
2019-09-29 01:07:38 +0200

    feat(page-layout): add countdown form date/time pickers and libraries
    closes #10

--- components/Countdown/CountdownForm/CountdownForm.tsx
+++ components/Countdown/CountdownForm/CountdownForm.tsx
@@ -1,5 +1,53 @@
+import React from 'react';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
+import Button from '@material-ui/core/Button';
+
 const CountdownForm: React.FC = () => {
-  return <>CountdownForm</>;
+  const [selectedDate, setSelectedDate] = React.useState(new Date());
+
+  const handleDateChange = date => {
+    setSelectedDate(date);
+  };
+
+  return (
+    <>
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <form>
+          <KeyboardDatePicker
+            margin="normal"
+            label="Date picker dialog"
+            format="MM/dd/yyyy"
+            value={selectedDate}
+            fullWidth
+            inputVariant="outlined"
+            onChange={handleDateChange}
+            KeyboardButtonProps={{
+              'aria-label': 'change date',
+            }}
+          />
+          <KeyboardTimePicker
+            margin="normal"
+            label="Time picker"
+            value={selectedDate}
+            fullWidth
+            inputVariant="outlined"
+            onChange={handleDateChange}
+            KeyboardButtonProps={{
+              'aria-label': 'change time',
+            }}
+          />
+          <Button variant="contained" color="primary" type="submit" fullWidth>
+            Start
+          </Button>
+        </form>
+      </MuiPickersUtilsProvider>
+    </>
+  );
 };
 
 export default CountdownForm;
